2fc32795d11c0950d31320a70d6773ed0aed319d
Zohar Jackson <zoharj@gmail.com>
2018-12-22 16:10:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# SaneMemo
+An open-source, well documented, spaced repetition algorithm.
+
+
+
+#### Features
+- Introduces sub-decks for efficiently learning large decks (Super useful!)
+- Intuitive variable names and algorithm parameters.
+- Fully open-source with human-readable examples.
+- Easily configurable parameters to accommodate for different users' memorization abilities. 
+- Computationally cheap to compute next card. No need to run a computation on every card in the deck. 
+
+#### Terminology
+- `card: an item to be memorized`
+- `deck: a set of cards`
+- `sub-deck: a subset of a deck. This is generated dynamically by the algorithm.`
+
+#### What are sub-decks?
+The use case of sub-decks is as follows. A user adds a large number of cards to 
+their deck (Ex: 100+) and now attempts to study. In order to facilitate efficient learning it
+is imperative to repeat the new cards with a frequency greater than 1/100. To solve this problem SaneMemo 
+groups new cards into sub-decks of a configurable size `default sub_deck_size=20`.
+
+
+#### Core Ideas
+Users is given three options to describe the difficulty of a card.
+- I KNOW IT
+- I KNOW IT BUT WILL FORGET
+- DON'T KNOW
+
+These are roughly equivalent to easy, medium and hard. But are more intuitive, 
+and more accurately model the human learning process.
+
+
+#### Algorithm
+See code.py
+
+        
+#### Inspiration
+    - SuperMemo2
+    - http://www.blueraja.com/blog/477/a-better-spaced-repetition-learning-algorithm-sm2
+    
+    
---
+++ code.py
@@ -0,0 +1,78 @@
+import random
+import datetime
+
+# The number of times needed for the user to get the card correct(EASY) consecutively before removing the card from
+# the current sub_deck.
+CONSECUTIVE_CORRECT_TO_REMOVE_FROM_SUBDECK_WHEN_KNOWN = 2
+CONSECUTIVE_CORRECT_TO_REMOVE_FROM_SUBDECK_WHEN_WILL_FORGET = 3
+
+# The number of cards in the sub-deck
+SUBDECK_SIZE = 15
+REMINDER_RATE = 1.6
+
+class Deck(object):
+
+    def __init__(self):
+        self.cards = []
+
+        # Used to make sure we don't display the same card twice
+        self.last_card = None
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def get_next_card(self):
+        self.cards = sorted(self.cards)  # Sorted by next_practice_time
+        sub_deck = self.cards[0:min(SUBDECK_SIZE, len(self.cards))]
+        card = random.choice(sub_deck)
+
+        # In case card == last card lets select again. We don't want to show the same card two times in a row.
+        while card == self.last_card:
+            card = random.choice(sub_deck)
+
+        self.last_card = card
+        return card
+
+
+class Card(object):
+
+    def __init__(self, front, back):
+        self.front = front
+        self.back = back
+
+        self.next_practice_time = datetime.utc.now()
+        self.consecutive_correct_answer = 0
+        self.last_time_easy = datetime.utc.now()
+
+    def update(self, performance_str):
+        """ Updates the card after the user has seen it and answered how difficult it was. The user can provide one of
+        three options: [I_KNOW, KNOW_BUT_WILL_FORGET, DONT_KNOW].
+        """
+
+        if performance_str == "KNOW_IT":
+            self.consecutive_correct_answer += 1
+
+            if self.consecutive_correct_answer >= CONSECUTIVE_CORRECT_TO_REMOVE_FROM_SUBDECK_WHEN_KNOWN:
+                days_since_last_easy = (datetime.utc.now() - self.last_time_easy).days
+                days_to_next_review = (days_since_last_easy + 2) * REMINDER_RATE
+                self.next_practice_time = datetime.utc.now() + datetime.time(days=days_to_next_review)
+                self.last_time_easy = datetime.utc.now()
+            else:
+                self.next_practice_time = datetime.utc.now()
+
+        elif performance_str == "KNOW_BUT_WILL_FORGET":
+            self.consecutive_correct_answer += 1
+
+            if self.consecutive_correct_answer >= CONSECUTIVE_CORRECT_TO_REMOVE_FROM_SUBDECK_WHEN_WILL_FORGET:
+                self.next_practice_time = datetime.utc.now() + datetime.time(days=1)
+            else:
+                self.next_practice_time = datetime.utc.now()
+
+        elif performance_str == "DONT_KNOW":
+            self.consecutive_correct_answer = 0
+            self.next_practice_time = datetime.utc.now()
+
+    def __cmp__(self, other):
+        """Comparator or sorting cards by next_practice_time"""
+        if hasattr(other, 'next_practice_time'):
+            return self.number.__cmp__(other.next_practice_time)
